ecb9550916ce10a7ae352cf970274d3aa2104666
Dimas Rinaldi <dimasrinaldi@gmail.com>
2025-08-03 19:49:49 +0700

    init

---
+++ app/root.tsx
@@ -0,0 +1,60 @@
+import { RefineThemes } from "@refinedev/antd";
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Scripts,
+  ScrollRestoration
+} from "@remix-run/react";
+
+import resetStyle from "@refinedev/antd/dist/reset.css";
+import { App as AntdApp, Button, ConfigProvider } from "antd";
+
+import { useEffect, useState } from "react";
+
+export const meta: MetaFunction = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix + Refine App",
+    viewport: "width=device-width,initial-scale=1",
+  },
+];
+
+export default function App(): JSX.Element {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ConfigProvider theme={RefineThemes.Purple}>
+          <AntdApp>
+            <ClientOnly>
+              <Button>Coba halobandun keren sekali</Button>
+            </ClientOnly>
+          </AntdApp>
+        </ConfigProvider>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: resetStyle }];
+}
+
+function ClientOnly({ children }: { children: React.ReactNode }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+  return <>{children}</>;
+}
